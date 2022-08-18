b0afde88c9bcdb408c33c21d63545392fe415883
HeJ <dark199174@gmail.com>
2022-08-18 22:08:44 +0900

    Implement [game_initialize] game initilizing code

---
+++ src/gameStart/initializeGame.js
@@ -0,0 +1,22 @@
+import updateScene from "./updateScene";
+import { saveData, clearData, getData } from "../utils/store";
+
+export default function initializeGame() {
+  // 초기화
+  clearData();
+  saveData("stageLevel", 1);
+
+  // Hero
+  const hero = { x: 0, y: 0, z: 0 };
+  saveData("hero", hero);
+
+  // Map
+  const map = ["00000000"];
+  saveData("map", map);
+
+  console.log(getData("hero"));
+  console.log(getData("stageLevel"));
+  console.log(getData("map"));
+
+  updateScene();
+}
