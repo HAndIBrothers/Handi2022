cceaa2d8e200f7082800af40b6d37c242062da36
HeJ <dark199174@gmail.com>
2022-08-23 22:03:27 +0900

    Implement [hero_render] render hero on screen

--- src/hero/components/DrawHero.js
+++ src/hero/components/DrawHero.js
@@ -1,17 +1,18 @@
+import "../styles/hero.css";
 // import hero width and height
 
-export default function drawHero(x, y, z) {
+export default function DrawHero(x, y, z) {
   const heroWidth = 200;
   const heroHeight = 200;
-
   return `
-  <div class=cube hero
-  style='tranform: scaleX(${heroWidth}) scaleY(${heroHeight})
-  translate3d(${x * 200}px, ${y * 200}px, ${z * 200}px)'>
+  <div class='hero'
+  style='transform: scaleX(${0.9}) scaleY(${0.9})
+  translate3d(${x * 200}px, ${y * 200}px, ${z * 200}px);'>
     <div class="face up"></div>
     <div class="face left"></div>
     <div class="face back"></div>
     <div class="face right"></div>
     <div class="face front"></div>
-  </div>`;
+  </div>
+  `;
 }
--- src/hero/components/RenderHero.js
+++ src/hero/components/RenderHero.js
@@ -1,4 +1,4 @@
-import drawHero from "../drawHero";
+import DrawHero from "./DrawHero";
 export default function (target) {
-  target.insertAdjacentHTML("beforeEnd", drawHero(1, 1, 1));
+  target.insertAdjacentHTML("beforeEnd", DrawHero(1, 1, 1));
 }
