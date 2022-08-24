d05d6152768e1a07b38b40dad4447a270e37f5be
ryusiin <risnworlds@gmail.com>
2022-08-24 19:39:18 +0900

    Merge

--- main.js
+++ main.js
@@ -1,14 +1,4 @@
-import { dataMap } from "./src/base/constants/constantMap";
-import { viewportSize } from "./src/base/constants/constant";
 import "./style.css";
 import initializeGame from "./src/gameStart/initializeGame";
-import { renderScene } from "./src/scene/components/RenderScene";
-import { renderFloor } from "./src/map/components/RenderFloor";
-import { renderMap } from "./src/map/components/RenderMap";
-import { renderCamera } from "./src/camera/components/RenderCamera";
 
 initializeGame();
-renderScene(viewportSize.width, viewportSize.height);
-renderFloor(dataMap[1].width, dataMap[1].height);
-renderMap(dataMap[1]);
-renderCamera(dataMap[1].start.x, dataMap[1].start.y, 1);
--- src/gameStart/updateScene.js
+++ src/gameStart/updateScene.js
@@ -1,4 +1,10 @@
 import RenderHero from "../hero/components/RenderHero";
+import { dataMap } from "../.././src/base/constants/constantMap";
+import { viewportSize } from "../.././src/base/constants/constant";
+import { renderScene } from "../.././src/scene/components/RenderScene";
+import { renderFloor } from "../.././src/map/components/RenderFloor";
+import { renderMap } from "../.././src/map/components/RenderMap";
+import { renderCamera } from "../.././src/camera/components/RenderCamera";
 
 export default function updateScene() {
   let t = 0;
@@ -13,8 +19,16 @@ export default function updateScene() {
     t++;
     $scene.style.width = $floor.style.width = 10 * 200 + "px";
     $scene.style.height = $floor.style.height = 10 * 200 + "px";
+
+    // Test Start
+    renderScene(viewportSize.width, viewportSize.height);
+    renderFloor(dataMap[1].width, dataMap[1].height);
+    renderMap(dataMap[1]);
+    renderCamera(dataMap[1].start.x, dataMap[1].start.y, 1);
+    // Test End
+
+    console.warn("Check"); //
     requestAnimationFrame(loop);
   };
-
-  requestAnimationFrame(loop);
+  loop();
 }
