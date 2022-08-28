4a4f8be51c614981faa4e876cf2fc0b309a2fa1a
ryusiin <risnworlds@gmail.com>
2022-08-28 13:43:05 +0900

    Check >>
    - Add Reset

--- src/gameStart/updateScene.js
+++ src/gameStart/updateScene.js
@@ -5,30 +5,31 @@ import { renderScene } from "../.././src/scene/components/RenderScene";
 import { renderFloor } from "../.././src/map/components/RenderFloor";
 import { renderMap } from "../.././src/map/components/RenderMap";
 import { renderCamera } from "../.././src/camera/components/RenderCamera";
+import { resetScene } from "../scene/ResetScene";
 
 export default function updateScene() {
   let t = 0;
   const $scene = document.querySelector("#scene");
   const $floor = document.querySelector("#floor");
+
+  // :: Test Start
+  resetScene();
+  renderScene(viewportSize.width, viewportSize.height);
   RenderHero($scene);
+  renderFloor(dataMap[1].width, dataMap[1].height);
+  renderMap(dataMap[1]);
+  renderCamera(dataMap[1].start.x, dataMap[1].start.y, 0.5);
+  // :: Test End
+
   const loop = () => {
     // 작동 확인을 위한 코드
     if (t % 1000 === 0) {
       console.log(t);
     }
     t++;
-    $scene.style.width = $floor.style.width = 10 * 200 + "px";
-    $scene.style.height = $floor.style.height = 10 * 200 + "px";
-
-    // Test Start
-    renderScene(viewportSize.width, viewportSize.height);
-    renderFloor(dataMap[1].width, dataMap[1].height);
-    renderMap(dataMap[1]);
-    renderCamera(dataMap[1].start.x, dataMap[1].start.y, 0.5);
-    // Test End
 
     console.warn("Check"); //
-    //requestAnimationFrame(loop);
+    requestAnimationFrame(loop);
   };
   loop();
 }
