b114505853a32cdc0fd93b57596981e6a85c669f
ryusiin <risnworlds@gmail.com>
2022-09-10 11:20:02 +0900

    Merge
    - Set Camera

--- src/camera/setCamera.js
+++ src/camera/setCamera.js
@@ -2,12 +2,12 @@ import { tileSize, viewportSize, zoomBase } from "../base/constants/constant";
 
 export const setCamera = (_x, _y, _z) => {
   scene.style.transformOrigin = `0% 0%`;
-  const xPos = -tileSize * _x + -tileSize;
-  const yPos = -tileSize * _y + -tileSize;
-  const zPos = -tileSize * _z + -tileSize;
+  const xPos = -tileSize * _x + -tileSize / 2;
+  const yPos = -tileSize * (_y - 1) + -tileSize;
+  const zPos = -tileSize * (_z + 5) + -tileSize;
 
-  const transform = `translate3d(${xPos}px, ${yPos}px, ${zPos}px) rotateX(10deg) rotateZ(0deg)`;
+  const transform = `translate3d(${xPos}px, ${yPos}px, ${zPos}px) rotateX(15deg) rotateZ(0deg)`;
   scene.style.transform = transform;
 
-  viewport.style.perspective = "0px";
+  viewport.style.perspective = "-10px";
 };
