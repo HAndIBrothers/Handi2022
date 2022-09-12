004a6a5e16055684ba8c88d8a3aee5701c16b7f2
ryusiin <risnworlds@gmail.com>
2022-09-12 18:31:31 +0900

    Fix
    - Fix Balance Speed

--- src/map/components/MoveMap.js
+++ src/map/components/MoveMap.js
@@ -30,7 +30,7 @@ export const MoveMap = (_movingCubes) => {
       data.cube[index].pos.x = Math.floor(Math.random() * 20 - 5);
       data.cube[index].pos.y = 2;
       data.cube[index].pos.z = Number(data.cube[index].pos.z) + 8;
-      data.cube[index].speed = Number(data.cube[index].speed) + 0.001;
+      data.cube[index].speed = Number(data.cube[index].speed) + 0.1;
 
       if (Number(data.cube[index].pos.z) > 10) {
         const colors = getColors(
